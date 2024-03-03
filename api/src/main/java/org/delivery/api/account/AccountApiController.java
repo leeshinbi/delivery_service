09675200adde0cc8366095f0f3af256bcf1119e1
leeshinbi <tlsql6831@naver.com>
2024-03-03 22:25:42 +0900

    api 공통 스펙 작동 테스트

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -2,6 +2,7 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +20,13 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me(){
-
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> me(){
+        var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
-    }
-    @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+
+        return Api.OK(response);
     }
 }

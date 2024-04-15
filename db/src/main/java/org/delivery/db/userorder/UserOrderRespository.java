612b641a46273f6ddfaf0be1f90917cd43dae4e4
leeshinbi <tlsql6831@naver.com>
2024-04-15 17:56:40 +0900

    유저 주문 엔티티 설정

---
+++ db/src/main/java/org/delivery/db/userorder/UserOrderRespository.java
@@ -0,0 +1,8 @@
+package org.delivery.db.userorder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserOrderRespository extends JpaRepository<UserOrderEntity, Long> {
+
+
+}
